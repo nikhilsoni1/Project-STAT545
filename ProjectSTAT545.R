52e83968f147df27e80b52a4a89b63a406b349f1
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2018-12-11 19:57:27 -0500

    Change of Plans

--- ProjectSTAT545.R
+++ ProjectSTAT545.R
@@ -92,78 +92,20 @@ pager<-function(root, seed.record=FALSE, seed.val=NULL)
 }
 
 
-MHalgo<-function(T.mat, X, perm, run=5000, verbose=F, B=2000)
+MHalgo<-function(a,b)
 {
-  run<-run+B
-  CTR<-1
-  BURNT<-F
-  MSG<-NULL
-  lh<-list()
-  perm.list<-list()
-  while(CTR<=run)
-  {
-    if(BURNT)
+  
+  beta<-(0+min(a))
+  print(beta/b)
+  acc<-beta/b
+  U<-runif(1)
+  for(i in 1:395){
+    if(U<acc)
     {
-      acc<-acceptance(T.mat, X, perm)
-      U<-runif(1)
-      if(acc$acc != 1)
-      {
-        perm<-acc$new_perm
-        MSG<-acc$new_msg
-        lh<-c(lh, likelihood(T.mat, X, perm)$ll)
-      }
-      else
-      {
-        perm<-acc$old_perm
-        MSG<-acc$old_msg
-        lh<-c(lh, likelihood(T.mat, X, perm)$ll)
-      }
-      perm.list[[CTR-B]]<-perm
-      if((CTR %% 100) == 0 && verbose)
-      {
-        print(CTR-B)
-        print(paste(MSG[1:20], collapse = ''))
-      }
+      random_sample[[i]]<-
     }
-    else
-    {
-      acc<-acceptance(T.mat, X, perm)
-      U<-runif(1)
-      if(acc$acc !=1)
-      {
-        perm<-acc$new_perm
-      }
-      else
-      {
-        perm<-acc$old_perm
-      }
-    }
-    if(CTR==B)
-    {
-      BURNT<-T
-    }
-    CTR<-CTR+1
   }
-  lh<-unlist(lh)
-  MSG<-paste(MSG, collapse = "")
-  return(list(message=MSG, likelihood=lh, permutations=perm.list))
 }
-likelihood<-function(t)
-{
-  freq[freq<0]<-1e-03
-  transition.matrix.result<-list(ll=sum(freq * log(t)))
-  return(transition.matrix.result)
-}
-acceptance<-function(t)
-{
-  rand_perm<-random_perm(perm)
-  a<-likelihood(T.mat, X, rand_perm)
-  b<-likelihood(T.mat, X, perm)
-  acc<-min(1, (a$ll/b$ll))
-  return(list(new_perm=rand_perm, new_msg=a$msg, old_msg=b$msg, old_perm=perm, acc=acc))
-}
-
-
 
 
 # operations----
@@ -177,22 +119,19 @@ for (i in 1:1000)
   a<-pager(goto)
   burn_in1[[i]]<-a
   goto<-a$url
-  print(i)
 }
 
-D_k<-burn_in[11:30]
-D_k_sample<-sample(D_k, 1)
-urls<-unique(unlist(list_feature(D_k, "url")))
-
+D_k<-burn_in1[100:663]
+D_k.urls<-unique(unlist(list_feature(D_k, "url")))
 new_page<-list()
 for(i in 1:500)
 {
-  new_page[[i]]<-pager(sample(urls,1))
+  new_page[[i]]<-pager(sample(D_k.urls,1))
   print(i)
 }
-
-
-freq <- unclass(table(unlist(list_feature(new_page, "url"))))
+freq<-unclass(table(unlist(list_feature(new_page, "url"))))
 t<-freq/500
 
-
+X<-burn_in1[100:663]
+freq.X<-unclass(table(unlist(list_feature(X, "url"))))
+t.x<-freq.X/length(X)
